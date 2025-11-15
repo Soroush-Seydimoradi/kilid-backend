bf7ab54c0567479b3303a10c2151f54698028fd8
Soroush <soroushseyedimoradi@gamil.com>
2025-11-15 11:50:21 +0330

    initial spring boot project

---
+++ src/main/java/com/kilid/KilidApplication.java
@@ -0,0 +1,12 @@
+package com.kilid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KilidApplication.class, args);
+    }
+}
