40612ba901218e66f9c69b21d6044b8704d01e77
Soroush <soroushseyedimoradi@gamil.com>
2026-02-16 11:03:20 +0330

    Refactor location and property: geo query added

--- src/main/java/com/kilid/model/Location.java
+++ src/main/java/com/kilid/model/Location.java
@@ -2,21 +2,26 @@ package com.kilid.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import java.util.*;
+import org.locationtech.jts.geom.Point;
+
+import java.util.List;
 
 @Entity
-@Getter
-@Setter @NoArgsConstructor @AllArgsConstructor @Builder
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Location {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id ;
+    private Long id;
 
-    private String city ;
-    private String distract ;
-    private String neighborhood ;
+    private String city;
+    private String district;
+    private String neighborhood;
 
-    @OneToMany(mappedBy = "location" ,cascade = CascadeType.ALL)
-    private List<Property> properties ;
+
+    @Column(columnDefinition = "geometry(Point,4326)")
+    private Point coordinates;
+
+    @OneToMany(mappedBy = "location", cascade = CascadeType.ALL)
+    private List<Property> properties;
 }
